470b565adca6e41aef7ee6944ab52cbf6bbfc85c
outman2023 <2876141934@qq.com>
2023-08-10 14:04:21 +0800

    8-10网关

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/GulimallCouponApplication.java
@@ -1,8 +1,10 @@
-package com.atguigu.gulimall.gulimallcoupon;
+package com.atguigu.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
 
